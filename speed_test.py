89afb3e6c0048db92ceff713339edc0e722eb019
Hassan Naik <hassannaik1989@gmail.com>
2024-10-31 15:20:13 +0000

    added comments to code and did changes before submitting assignments

--- speed_test.py
+++ speed_test.py
@@ -3,13 +3,35 @@ import numpy as np
 from  ydata_profiling import ProfileReport
 
 df = pd.read_excel("results2024-10-24-1512.xlsx")
+# reads excel file
 
 df = df. set_index("Unnamed: 0")
+# Sets index
+df = df.drop(columns=["Date-time"])
+# Removes column 
+
+# print(df)
+# prints data frame with index and column removed
+
+# print(df.describe())
+# returns 5 figure summary
+
+# print(df.info())
+# The number of entries (rows) and Each column's name and data type
 
 # print(f"{df['Download (Mb/s)'].mean():.2f} mean downlad")
+# prints mean download speed
+
+
 # print(f"{df['Download (Mb/s)'].median():.2f} median download")
+# prints median download speed
+
 # print(f"{df['Upload (Mb/s)'].mean():.2f} mean upload")
+# prints mean upload speed
+
 # print(f"{df['Upload (Mb/s)'].median():.2f} median upload")
+# prints median upload speed
+
  
 # Mean of the download speeds
 # avg = df["Download (Mb/s)"].mean()
@@ -29,11 +51,6 @@ df = df. set_index("Unnamed: 0")
 # def fast():
 #     if "Download (Mb/s)" <= avg:
 
-# print(df)
-# print(df[])
-# print(df.describe)
-# print(df.info())
-
 # df = df.sort_values(by='Upload (Mb/s)', ascending=True)
 # print(df)
 # df = df.sort_values(by='Upload (Mb/s)', ascending=False)
@@ -51,17 +68,21 @@ df = df. set_index("Unnamed: 0")
 x = df[df['Download (Mb/s)'] >df['Download (Mb/s)'].mean()]
 
 print(x)
+# prints download speeds greater than mean
    
-
-print(f"{df['Download (Mb/s)'].min():.2f} slowest downlad")
-print(f"{df['Download (Mb/s)'].max():.2f} fastest download")
-print(f"{df['Upload (Mb/s)'].min():.2f} slowest upload")
-print(f"{df['Upload (Mb/s)'].max():.2f} fastest upload")
+# print(f"{df['Download (Mb/s)'].min():.2f} slowest download")
+# prints slowest download speed
+# print(f"{df['Download (Mb/s)'].max():.2f} fastest download")
+# prints fastest download speed
+# print(f"{df['Upload (Mb/s)'].min():.2f} slowest upload")
+# prints slowest upload speed
+# print(f"{df['Upload (Mb/s)'].max():.2f} fastest upload")
+# prints fastest upload speed
 
 # national average download speed in the UK is 73.21 Mbps, 
 # while the average upload speed is 18.4 Mbps
 
-a = 73.21
+# a = 73.21
 # float(b) = 18.40
 
 df1 = pd.read_excel("group_results.xlsx")
@@ -69,23 +90,24 @@ df1 = pd.read_excel("group_results.xlsx")
 # df = df. set_index("Internet Service Provider")
 
 df1 = df1.drop(columns=["Package (if known)"])
+# removes as was mainly empty
 
 df1 = df1.dropna(how="any" )
+# remove any empty rows
 
-print(f"{df1['Mean Download'].mean():.2f} mean downlad of group")
+# print(f"{df1['Mean Download'].mean():.2f} mean download of group")
+# prints mean of group download
+# print(f"{df1['Mean Upload'].mean():.2f} mean upload of group")
+# prints mean of groups upload
+# print(f"{df1['Mean Download'].min():.2f} slowest download of group")
+# prints slowest download speed
+# print(f"{df1['Mean Download'].max():.2f} fastest download of group")
+# prints fastest download speed
+# print(f"{df1['Mean Upload'].min():.2f} slowest upload of group")
+# prints slowest upload speed
+# print(f"{df1['Mean Upload'].max():.2f} fastest upload of group")
+# prints fastest upload speed
 
-print(f"{df1['Mean Upload'].mean():.2f} mean upload of group")
+# profile = ProfileReport(df1, title="Group Speed")
 
-print(f"{df1['Mean Download'].min():.2f} slowest downlad")
-print(f"{df1['Mean Download'].max():.2f} fastest download")
-print(f"{df1['Mean Upload'].min():.2f} slowest upload")
-print(f"{df1['Mean Upload'].max():.2f} fastest upload")
-
-# print(df1)
-# print(df1[])
-# print(df1.describe)
-# print(df1.info())
-
-profile = ProfileReport(df1, title="Group Speed")
-
-profile.to_file('gsr.html')
+# profile.to_file('gsr.html')
